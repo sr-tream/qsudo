18c9e949a8082823f3d099ce502a0b58ff5e572d
SR_team <sr-tream@yandex.ru>
2018-04-10 19:43:50 +0300

    Revert "remove old files"
    This reverts commit 5720479f1b0f917e9b9f1614c1be35ef18db4d71.

---
+++ qsudo.cpp
@@ -0,0 +1,77 @@
+#include "qsudo.h"
+
+Qsudo::Qsudo(int &argc, char **argv, QWidget *parent) :
+	QDialog(parent)
+{
+	setupUi(this);
+	sudo = new QProcess();
+
+	connect(sudo, &QProcess::readyReadStandardOutput, this, &Qsudo::parseOutput);
+	connect(sudo, QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished), this, &Qsudo::sudoFinished);
+
+	QStringList args;
+	args << "-S"; // Write the prompt to the standard error and read the password from the standard input instead of using the terminal device.
+	args << "-p"; // Use a custom password prompt with optional escape sequences.
+	args << "qsudogetpass"; // prompt
+	for (int i = 1; i < argc; ++i)
+		args << argv[i];
+
+	sudo->setArguments(args);
+	sudo->setEnvironment(QProcess::systemEnvironment());
+	sudo->setProcessChannelMode(QProcess::ProcessChannelMode::MergedChannels);
+	sudo->setProgram("sudo");
+	sudo->start();
+}
+
+void Qsudo::writePassword()
+{
+	sudo->write(pass->text().toLocal8Bit() + "\n");
+	pass->clear();
+}
+
+void Qsudo::parseOutput()
+{
+	QString stdout = sudo->readAllStandardOutput().toStdString().c_str();
+
+	if (stdout.indexOf("qsudogetpass") == 0){
+		if (!pass->text().isEmpty())
+			writePassword();
+		else {
+			awaitForPassword = true;
+			if (isHidden())
+				show();
+		}
+	}
+}
+
+void Qsudo::sudoFinished(int code, QProcess::ExitStatus)
+{
+	QApplication::exit(code);
+}
+
+void Qsudo::on_showPass_clicked()
+{
+	if (pass->echoMode() == QLineEdit::Password){
+                showPass->setIcon(QIcon(":/passShow.png"));
+		pass->setEchoMode(QLineEdit::Normal);
+	} else {
+                showPass->setIcon(QIcon(":/passHide.png"));
+		pass->setEchoMode(QLineEdit::Password);
+	}
+}
+
+void Qsudo::on_buttonBox_rejected()
+{
+	if (sudo)
+		sudo->kill();
+	close();
+}
+
+void Qsudo::on_buttonBox_accepted()
+{
+	if (!pass->text().isEmpty()){
+		if (awaitForPassword)
+			writePassword();
+		hide();
+	}
+}
